bea40b9cb0578f6b2943d93423240ee3b6b48232
Jonathan Stacks <jonstacks13@gmail.com>
2018-01-23 09:56:20 -0600

    Implement updated interface on providers

--- pkg/providers/aws/provider.go
+++ pkg/providers/aws/provider.go
@@ -5,15 +5,22 @@ import (
 	"github.com/c2fo/cloud-finder/pkg/cloudfinder/provider"
 )
 
+const name = "aws"
+
 // Provider is the AWS cloudfinder provider
 type Provider struct{}
+
+func init() {
+	cloudfinder.Register(name, &Provider{})
+}
+
+// Name returns the provider name as needed by provider.Provider
+func (p *Provider) Name() string {
+	return name
+}
 
 // Check checks if we are in this cloud providers cloud. If so, we return the
 // result. Else, nil.
 func (p *Provider) Check(opts *provider.Options) provider.Result {
 	return nil
-}
-
-func init() {
-	cloudfinder.Register("aws", &Provider{})
 }
